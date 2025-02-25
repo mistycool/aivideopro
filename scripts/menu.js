767c57174c20a5d573bfe69cf1ab5ed06baf5c6a
mistycool <mistycool666@gmail.com>
2025-02-25 20:14:40 +0100

    Create menu.js

---
+++ scripts/menu.js
@@ -0,0 +1,9 @@
+// scripts/menu.js
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.querySelector('.menu-icon');
+  const menu = document.querySelector('.menu');
+
+  menuIcon.addEventListener('click', function () {
+    menu.classList.toggle('open');
+  });
+});
