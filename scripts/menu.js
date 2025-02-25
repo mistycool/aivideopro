530f9feb14da561f23e3ff9a21429f2488cc44b1
mistycool <mistycool666@gmail.com>
2025-02-25 22:42:25 +0100

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,7 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
   const menuIcon = document.querySelector('.menu-icon');
   const menu = document.querySelector('.menu');
 
+  // Toggle the side menu
   menuIcon.addEventListener('click', function () {
     menu.classList.toggle('open');
+  });
+
+  // Toggle sub-menus
+  const subMenuHeaders = document.querySelectorAll('.sub-menu-header');
+  subMenuHeaders.forEach(header => {
+    header.addEventListener('click', function () {
+      const subMenu = this.parentElement;
+      subMenu.classList.toggle('open');
+    });
   });
 });
